5f3bb100e7955b1e0ca9b8a8c72bf34ae2563894
SwimmingLee <swimming_7291@daum.net>
2019-11-18 22:46:41 +0900

    route update

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+import helmet from "helmet"
+import morgan from "morgan"
+import userRouter from "./routers/userRouter"
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+
+app.use('/user', userRouter);
+
+export default app;
---
+++ init.js
@@ -0,0 +1,6 @@
+//var express = require("express");
+import app from "./app";
+
+const PORT = 4000;
+
+app.listen(PORT);
